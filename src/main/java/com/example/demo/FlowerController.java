8b5807b5ee8f8d1f8acf4bcd7f58b72f9db04314
Sergii Dmytryshyn <116553071+sergiidmytryshyn@users.noreply.github.com>
2023-12-11 18:51:10 +0200

    Update FlowerController.java

--- src/main/java/com/example/demo/FlowerController.java
+++ src/main/java/com/example/demo/FlowerController.java
@@ -2,14 +2,12 @@ package com.example.demo;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.item.Flower;
 
-@SpringBootApplication
 @RestController
 @RequestMapping("/api/flowers")
 public class FlowerController {
